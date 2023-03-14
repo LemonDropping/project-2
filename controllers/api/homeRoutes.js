b163f2b88ddb4476e89df1207e07f350ee493f3d
Cole Roberts <coleroberts76@gmail.com>
2023-03-13 19:18:06 -0500

    rewriting code for controller files

--- controllers/api/homeRoutes.js
+++ controllers/api/homeRoutes.js
@@ -1,12 +1,5 @@
 const router = require('express').Router;
 const { all } = require('./login')
 
-router.get ('/', async (req, res) => {
-    try {
-        const loginData = await all();
-        res.json(data);
-    } catch (error) {
-        console.error (error);
-        res.status(500).send('Internal Server Error');
-    }
-});
+
+
