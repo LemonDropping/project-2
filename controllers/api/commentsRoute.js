14359cdaa00fd7f93dd5aebc0d6fc7d718dc51d6
Eric Reyes <ericreyes1109@gmail.com>
2023-03-18 15:34:33 -0500

    added authentication to routes

--- controllers/api/commentsRoute.js
+++ controllers/api/commentsRoute.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const { Comment } = require('../../models/comment');
+const ifLoggedIn = require('../../utils/auth');
 
-router.get('/comments/:gameId', async (req, res) => {
+router.get('/comments/:gameId', ifLoggedIn, async (req, res) => {
     try {
     } catch (error) {
         res.status(500).json({ message: 'Comments not available'});
     }
 });
 
-router.post('/comments/game:Id', async (req, res) => {
+router.post('/comments/game:Id', ifLoggedIn, async (req, res) => {
     try {
     } catch (error) {
        res.status(500).json({ message: 'Could not create comment'})
