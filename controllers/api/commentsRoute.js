ae0b4b804a6b7152217f09bdd3a310a03224d4ee
Eric Reyes <ericreyes1109@gmail.com>
2023-03-18 19:17:43 -0500

    finished the comments routes

--- controllers/api/commentsRoute.js
+++ controllers/api/commentsRoute.js
@@ -1,20 +1,41 @@
 const express = require('express');
 const router = express.Router();
-const { Comment } = require('../../models/Comment');
+const { Comment } = require('../../models/');
 const withAuth = require('../../utils/auth');
-
-router.get('/comments/:gameId', withAuth, async (req, res) => {
-    try {
-    } catch (error) {
-        res.status(500).json({ message: 'Comments not available'});
-    }
+// getting all comments
+router.get('/',  async (req, res) => {
+    Comment.findAll({}).then(dbComment => res.json(dbComment))
+    .catch(err => {
+        res.status(500). json(err);
+    });
+});
+// posting a comment when logged in
+router.post('/:Id', withAuth, async (req, res) => {
+    Comment.create({
+        content: req.body.content,
+        user_id: req.session.user_id,
+        game_id: req.body.game_id,
+    }).then(dbComment => res.json(dbComment))
+    .catch(err => {
+        res.status(500).json(err);
+    });
 });
 
-router.post('/comments/game:Id', withAuth, async (req, res) => {
-    try {
-    } catch (error) {
-       res.status(500).json({ message: 'Could not create comment'})
-    }
+// deleting a comment
+router.delete('/:id', withAuth, (req, res) => {
+    Comment.destroy({
+        where: {
+            user_id: req.params.user_id
+        }
+    }).then(dbComment => {
+        if(!dbComment) {
+            res.status(404).json({ message: 'No comment found with ths ID.'});
+            return;
+        }
+        res.json(dbComment);
+    }).catch (err => {
+        res.status(404).json(err);
+    });
 });
 
 module.exports = router;
